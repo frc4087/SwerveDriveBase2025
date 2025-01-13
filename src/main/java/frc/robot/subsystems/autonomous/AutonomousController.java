a7729b4b16801c09dcad0f6db04ed65e0d2a1814
Allen <57648478+san4d@users.noreply.github.com>
2025-01-13 17:23:34 -0600

    Create PathPlannableSubsystem and AutonomousController structure (#4)
    * Create PathPlannable and AutonomousController structure
    
    * Rename PathPlannable to PathPlannableSubsystem

---
+++ src/main/java/frc/robot/subsystems/autonomous/AutonomousController.java
@@ -0,0 +1,37 @@
+package frc.robot.subsystems.autonomous;
+import edu.wpi.first.wpilibj.IterativeRobotBase;
+
+/**
+ * Describes the high-level behavior required to controll a robot's
+ * autonomous mode.
+ */
+public interface AutonomousController {
+
+    /**
+     * Initialization code for autonomous mode. Called each time the robot enters 
+     * autonomous mode.
+     * 
+     * See {@link IterativeRobotBase#autonomousInit()}
+     */
+    void runInit();
+
+    /**
+     * Periodically called during autonomous mode.
+     * 
+     * TODO: Look into what determines the period.
+     * 
+     * See {@link IterativeRobotBase#autonomousPeriodic()()}
+     */
+    void runPeriodic();
+
+    /**
+     * Exit code for autonomous mode. Called each time the robot exits 
+     * autonomous mode.
+     * 
+     * This method clears all commands created in autonomous mode in
+     * preparation for teleop.
+     * 
+     * See {@link IterativeRobotBase#autonomousExit()}
+     */
+    void runExit();
+}
