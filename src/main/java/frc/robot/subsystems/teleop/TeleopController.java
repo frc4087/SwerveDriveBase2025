881b836dc3de0d1723fd6ac42f397eede94ba17b
”Andrei” <andrei.chirila07121999@gmail.com>
2025-01-17 03:02:49 +0200

    Update comments as per requirements

--- src/main/java/frc/robot/subsystems/teleop/TeleopController.java
+++ src/main/java/frc/robot/subsystems/teleop/TeleopController.java
@@ -3,33 +3,33 @@ import edu.wpi.first.wpilibj.IterativeRobotBase;
 
 /**
  * Describes the high-level behavior required to controll a robot's
- * autonomous mode.
+ * teleop mode.
  */
 public interface TeleopController {
 
     /**
-     * Initialization code for autonomous mode. Called each time the robot enters 
-     * autonomous mode.
+     * Initialization code for teleop mode. Called each time the robot enters 
+     * teleop mode.
      * 
      * See {@link IterativeRobotBase#teleopInit()}
      */
     void runInit();
 
     /**
-     * Periodically called during autonomous mode.
+     * Periodically called during teleop mode.
      * 
      * TODO: Look into what determines the period.
      * 
-     * See {@link IterativeRobotBase#teleopPeriodic()()}
+     * See {@link IterativeRobotBase#teleopPeriodic()}
      */
     void runPeriodic();
 
     /**
-     * Exit code for autonomous mode. Called each time the robot exits 
-     * autonomous mode.
+     * Exit code for teleop mode. Called each time the robot exits 
+     * teleop mode.
      * 
-     * This method clears all commands created in autonomous mode in
-     * preparation for teleop.
+     * This method clears all commands created in teleop mode in
+     * preparation for disabled.
      * 
      * See {@link IterativeRobotBase#teleopExit()}
      */
