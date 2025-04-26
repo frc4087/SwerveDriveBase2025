dd105f61503678481396a6566d9fb8fb56861510
wyvern <wyvern4060ti@gmail.com>
2025-04-26 16:23:22 -0500

    stuff

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,16 +4,10 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.Filesystem;
-import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 import com.ctre.phoenix6.signals.NeutralModeValue;
-import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.cameraserver.CameraServer;
-import edu.wpi.first.net.WebServer;
 
 public class Robot extends TimedRobot {
 
@@ -21,28 +15,14 @@ public class Robot extends TimedRobot {
 
     public Robot() {
         m_robotContainer = new RobotContainer();
-        Timer.delay(5);
-        CameraServer.startAutomaticCapture();
     }
+
     @Override
-    public void robotInit() {
-        WebServer.start(5800, Filesystem.getDeployDirectory().getPath());
-        
-    }
+    public void robotInit() {}
+    
     @Override
     public void robotPeriodic() {
         CommandScheduler.getInstance().run();
-
-        // Read battery voltage
-        double voltage = RobotController.getBatteryVoltage();
-        // Send voltage to SmartDashboard
-        SmartDashboard.putNumber("Battery Voltage", voltage);
-
-         // Retrieve arm position
-        double armPosition = m_robotContainer.frankenArm.armMotor.getPosition().getValueAsDouble();
-        // Send arm position to SmartDashboard
-        SmartDashboard.putNumber("Arm Position", armPosition);
-
     }
 
     @Override
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,7 +9,6 @@ import static edu.wpi.first.units.Units.RadiansPerSecond;
 import static edu.wpi.first.units.Units.RotationsPerSecond;
 
 import com.ctre.phoenix6.hardware.TalonFX;
-import com.ctre.phoenix6.signals.NeutralModeValue;
 import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
 import com.ctre.phoenix6.swerve.SwerveRequest;
 
@@ -19,7 +18,6 @@ import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import frc.robot.commands.RotateBotCommand;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
 import frc.robot.subsystems.FrankenArm;
 import frc.robot.subsystems.KlimbKardashian;
@@ -43,8 +41,6 @@ public class RobotContainer {
 	
 	private final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
 	private final SwerveRequest.PointWheelsAt point = new SwerveRequest.PointWheelsAt();
-
-	private final Telemetry logger = new Telemetry(MaxSpeed);
 
 	public final CommandXboxController driverController = new CommandXboxController(0);
 
@@ -81,8 +77,6 @@ public class RobotContainer {
 
 		setUpDriverController();
 		setUpOpController();
-		setUpTelemetry();
-
 	}
 
 	private void setUpDriverController() {
@@ -183,10 +177,6 @@ public class RobotContainer {
 
 		// Climber Reset 
 		//operatorController.povLeft().whileTrue(klimbKardashian.restClimb());
-	}
-
-	private void setUpTelemetry() {
-		drivetrain.registerTelemetry(logger::telemeterize);
 	}
 
 	public AutonomousController auto() {
